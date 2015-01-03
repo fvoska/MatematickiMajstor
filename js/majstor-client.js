50e4d332c4792028db328655382ecd8974a61fc6
Filip Voska <filip.voska@gmail.com>
2015-01-03 20:54:13 +0100

    Changed username prompt with DB username.

--- js/majstor-client.js
+++ js/majstor-client.js
@@ -3,158 +3,174 @@ var myBegin;
 var myUsername;
 //var roomName;
 var playersList = [];
+var socket;
+var URLSessionTest = "http://localhost/MatematickiMajstor/server/php/test_session.php";
 
-// Sockets section, handles events from server.
-var socket = io.connect("127.0.0.1:8080");
-// Use 95.85.6.210:8080 for remote server if you don't run node.js on localhost.
-
-// On socket conenction.
-socket.on("connect", function() {
-    // Add user and prompt for name.
-    // Once we have DB in place we will use username from DB instead of a prompt.
-    myUsername = prompt("What's your name: ");
-    playersList.push(myUsername);
-    socket.emit("addUser", myUsername);
-});
-
-// Chat update
-socket.on("updateChat", function (username, data) {
-    // Append message to HTML.
-    $("#conversation").append('<div class="chatItem"><b>'+ username + ':</b> ' + data + '</div>');
-});
-
-// Update room list.
-socket.on("updateRooms", function (rooms, current_room) {
-    /*
-     rooms variable structure: array of JSONs
-     [{"roomName": "room1", "numberOfUsers": 3}, {"roomName": "room2", "numberOfUsers": 1},...]
-     */
-    // We always receive list of all room, so empty HTML before adding rooms.
-    $("#rooms").empty();
-    $.each(rooms, function(key, value) {
-        // value is JSON string, parse it into JSON object.
-        var roomJSON = JSON.parse(value);
-
-        // If there are no rooms except Lobby this will be null.
-        if (roomJSON != null) {
-            if (roomJSON.roomName == current_room) {
-                // Don't allow users to join room they are already in.
-                $("#rooms").append('<div class="roomItem">' + roomJSON.roomName + ' (' + roomJSON.numberOfUsers + '/4)</div>');
-            }
-            else {
-                // Create link with onlick function that switches rooms.
-                $("#rooms").append('<div class="roomItem"><a href="#" onclick="switchRoom(\'' + roomJSON.roomName + '\')">' + roomJSON.roomName + ' (' + roomJSON.numberOfUsers + '/4)</a></div>');
-            }
+$(document).ready(function() {
+    $.get(URLSessionTest, function(data) {
+        if (data == "") {
+            // Redirect to login.
+            var url = "login.html";
+            $(location).attr("href", url);
         }
-    });
-});
+        else {
+            // Get username.
+            myUsername = data;
 
-// Received new task
-socket.on("task", function(task) {
-    /* task structure: JSON
-     {
-     "task": "2 + 2",
-     "result": "4",
-     "suggestions": ["1", "2", "3"] <- this isn't yet implemented in server, will be soon.
-     }
-     */
+            // Sockets section, handles events from server.
+            socket = io.connect("127.0.0.1:8080");
+            // Use 95.85.6.210:8080 for remote server if you don't run node.js on localhost.
 
-    // Enable buttons.
-    $(".suggestion").prop("disabled", false);
-    $(".suggestion").css({"background-color": "#337ab7"});
+            // On socket conenction.
+            socket.on("connect", function() {
+                // Add user and prompt for name.
+                // Once we have DB in place we will use username from DB instead of a prompt.
+                //myUsername = prompt("What's your name: ");
+                playersList.push(myUsername);
+                socket.emit("addUser", myUsername);
+            });
 
-    // Reset user list colors.
-    $(".player").css({"color": "#337ab7"});
+            // Chat update
+            socket.on("updateChat", function (username, data) {
+                // Append message to HTML.
+                $("#conversation").append('<div class="chatItem"><b>'+ username + ':</b> ' + data + '</div>');
+            });
 
-    // Parse JSON.
-    var taskJSON = JSON.parse(task);
+            // Update room list.
+            socket.on("updateRooms", function (rooms, current_room) {
+                /*
+                 rooms variable structure: array of JSONs
+                 [{"roomName": "room1", "numberOfUsers": 3}, {"roomName": "room2", "numberOfUsers": 1},...]
+                 */
+                // We always receive list of all room, so empty HTML before adding rooms.
+                $("#rooms").empty();
+                $.each(rooms, function(key, value) {
+                    // value is JSON string, parse it into JSON object.
+                    var roomJSON = JSON.parse(value);
 
-    // Show task on webpage.
-    $("#task").animate({"opacity": "0"}, 250, null, function() {
-        $("#task").html(taskJSON.task);
-        $("#task").animate({"opacity": "1"}, 250);
-    });
+                    // If there are no rooms except Lobby this will be null.
+                    if (roomJSON != null) {
+                        if (roomJSON.roomName == current_room) {
+                            // Don't allow users to join room they are already in.
+                            $("#rooms").append('<div class="roomItem">' + roomJSON.roomName + ' (' + roomJSON.numberOfUsers + '/4)</div>');
+                        }
+                        else {
+                            // Create link with onlick function that switches rooms.
+                            $("#rooms").append('<div class="roomItem"><a href="#" onclick="switchRoom(\'' + roomJSON.roomName + '\')">' + roomJSON.roomName + ' (' + roomJSON.numberOfUsers + '/4)</a></div>');
+                        }
+                    }
+                });
+            });
 
-    // Add correct answer to suggestions array.
-    var suggestionsArray = [];
-    suggestionsArray.push(taskJSON.result);
+            // Received new task
+            socket.on("task", function(task) {
+                /* task structure: JSON
+                 {
+                 "task": "2 + 2",
+                 "result": "4",
+                 "suggestions": ["1", "2", "3"] <- this isn't yet implemented in server, will be soon.
+                 }
+                 */
 
-    // Add suggestions to suggestions array.
-    for (var i = 0; i < 3; i++) {
-        //suggestionsArray.push(i);
-        suggestionsArray.push(taskJSON.suggestions[i]);
-    }
+                // Enable buttons.
+                $(".suggestion").prop("disabled", false);
+                $(".suggestion").css({"background-color": "#337ab7"});
 
-    // Shuffle suggestions.
-    suggestionsArray = shuffle(suggestionsArray);
+                // Reset user list colors.
+                $(".player").css({"color": "#337ab7"});
 
-    // Show suggestions in "boxes" on webpage.
+                // Parse JSON.
+                var taskJSON = JSON.parse(task);
 
-    $(".suggestion").animate({"opacity": "0"}, 250, null, function() {
-        for (var i = 1; i <= 4; i++) {
-            $("#suggestion" + i).html(suggestionsArray[i-1]);
-        }
-        $(".suggestion").animate({"opacity": "1"}, 250);
-    });
+                // Show task on webpage.
+                $("#task").animate({"opacity": "0"}, 250, null, function() {
+                    $("#task").html(taskJSON.task);
+                    $("#task").animate({"opacity": "1"}, 250);
+                });
 
-    // Remember correct answer.
-    correctAnswer = taskJSON.result;
+                // Add correct answer to suggestions array.
+                var suggestionsArray = [];
+                suggestionsArray.push(taskJSON.result);
 
-    // Start timer.
-    myBegin = new Date().getTime();
-});
+                // Add suggestions to suggestions array.
+                for (var i = 0; i < 3; i++) {
+                    //suggestionsArray.push(i);
+                    suggestionsArray.push(taskJSON.suggestions[i]);
+                }
 
-// Round results.
-socket.on("roundResults", function(fastestPlayer, fastestTime, over) {
-    if (fastestPlayer == null) {
-        // Noone answered correctly in time.
-    }
-    else {
-        $(".progress-bar").each(function() {
-            var userTemp = $(this);
-            if (userTemp.html() == fastestPlayer)
-            {
-                // Increase progress bar for fastest player.
-                var progress = parseInt(userTemp.attr('aria-valuenow')) + 25;
-                userTemp.css('width', progress+'%').attr('aria-valuenow', progress);
-            }
-        });
-    }
-});
+                // Shuffle suggestions.
+                suggestionsArray = shuffle(suggestionsArray);
 
-// New user in room notification.
-socket.on("updateUsers", function(users) {
-    // Store list locally.
-    playersList = users;
-    console.log(playersList);
+                // Show suggestions in "boxes" on webpage.
 
-    // Clear DOM.
-    $("#players").empty();
-    $("#progressContainer").empty();
+                $(".suggestion").animate({"opacity": "0"}, 250, null, function() {
+                    for (var i = 1; i <= 4; i++) {
+                        $("#suggestion" + i).html(suggestionsArray[i-1]);
+                    }
+                    $(".suggestion").animate({"opacity": "1"}, 250);
+                });
 
-    // Insert each player in DOM.
-    for (var i = 0; i < playersList.length; i++) {
-        $("#players").append('<div class="player" id="player' + i + '">' + playersList[i] + '</div>');
-        $("#progressContainer").append('<div class="progress">' +
-                                           '<div id="progress' + i + '" class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">' + playersList[i] + '</div>' +
-                                       '</div>');
-    }
-});
+                // Remember correct answer.
+                correctAnswer = taskJSON.result;
 
-// Feedback from other player answers.
-socket.on("someoneAnswered", function(user, result, time) {
-    // Find element of that player
-    $(".player").each(function() {
-        var userTemp = $(this);
-        if (userTemp.html() == user)
-        {
-            // Color it according to wrong/right answer.
-            if (result == "T") {
-                userTemp.css({"color": "green"});
-            }
-            else if (result == "F") {
-                userTemp.css({"color": "red"});
-            }
+                // Start timer.
+                myBegin = new Date().getTime();
+            });
+
+            // Round results.
+            socket.on("roundResults", function(fastestPlayer, fastestTime, over) {
+                if (fastestPlayer == null) {
+                    // Noone answered correctly in time.
+                }
+                else {
+                    $(".progress-bar").each(function() {
+                        var userTemp = $(this);
+                        if (userTemp.html() == fastestPlayer)
+                        {
+                            // Increase progress bar for fastest player.
+                            var progress = parseInt(userTemp.attr('aria-valuenow')) + 25;
+                            userTemp.css('width', progress+'%').attr('aria-valuenow', progress);
+                        }
+                    });
+                }
+            });
+
+            // New user in room notification.
+            socket.on("updateUsers", function(users) {
+                // Store list locally.
+                playersList = users;
+                console.log(playersList);
+
+                // Clear DOM.
+                $("#players").empty();
+                $("#progressContainer").empty();
+
+                // Insert each player in DOM.
+                for (var i = 0; i < playersList.length; i++) {
+                    $("#players").append('<div class="player" id="player' + i + '">' + playersList[i] + '</div>');
+                    $("#progressContainer").append('<div class="progress">' +
+                    '<div id="progress' + i + '" class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style="width: 25%;">' + playersList[i] + '</div>' +
+                    '</div>');
+                }
+            });
+
+            // Feedback from other player answers.
+            socket.on("someoneAnswered", function(user, result, time) {
+                // Find element of that player
+                $(".player").each(function() {
+                    var userTemp = $(this);
+                    if (userTemp.html() == user)
+                    {
+                        // Color it according to wrong/right answer.
+                        if (result == "T") {
+                            userTemp.css({"color": "green"});
+                        }
+                        else if (result == "F") {
+                            userTemp.css({"color": "red"});
+                        }
+                    }
+                });
+            });
         }
     });
 });
